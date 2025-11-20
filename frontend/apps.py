9dca157ae3cd2a1f4a538fcfbb330895126a50de
Viresh Soedhwa <vsoedhwa@bcit.ca>
2025-11-20 13:52:11 -0800

    fix: remove unused version and image environment variables from docker-compose and logging

--- frontend/apps.py
+++ frontend/apps.py
@@ -13,9 +13,6 @@ class FrontendConfig(AppConfig):
         if 'runserver' in sys.argv or 'qconweb.asgi:application' in sys.argv:
             logger.info("API_HOST: " + os.environ.get('API_HOST'))
             logger.info("API_PORT: " + os.environ.get('API_PORT'))
-            logger.info("APP_VERSION: " + os.environ.get('APP_VERSION'))
-            logger.info("IMAGE_TAG: " + os.environ.get('IMAGE_TAG'))
-            logger.info("IMAGE_NAME: " + os.environ.get('IMAGE_NAME'))
             logger.info("qconweb has started")
             
             if 'runserver' in sys.argv:
