b0f57255fc188f9c5beabc9862b4badc08eb0c8c
Arvin Rolos <arolos@bcit.ca>
2026-01-28 09:42:22 -0800

    fix: Upgrade qcon-web to Django 6 and Python 3.14, bump deps and runtime versions

--- qconweb/settings.py
+++ qconweb/settings.py
@@ -70,6 +70,7 @@ CSRF_COOKIE_HTTPONLY = True
 
 INSTALLED_APPS = [
     'channels',
+    'daphne',
     # 'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -157,8 +158,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
-USE_L10N = True
 
 USE_TZ = True
 
